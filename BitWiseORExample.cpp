68ba637b900c1dc9030e49e8f50a18bbb63c9973
kdowd <kevin.dowd@gmail.com>
2024-10-18 11:36:30 +1300

    hello

--- BitWiseORExample.cpp
+++ BitWiseORExample.cpp
@@ -8,13 +8,12 @@ using namespace std;
 
 int main() {
 
-
+	// hello
 	int a = 12, b = 25;
 	cout << "a = " << a << endl;
 	cout << "b = " << b << endl;
 	cout << "a | b = " << (a | b) << endl;
 
 	return 0;
-
 
 }
