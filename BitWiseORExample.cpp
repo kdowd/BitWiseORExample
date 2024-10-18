694d9daed151ad56d6272b5c3abf795b89415936
kartikgoel598 <kartikgoel598@gmail.com>
2024-10-18 13:26:13 +1300

    kartik branch

--- BitWiseORExample.cpp
+++ BitWiseORExample.cpp
@@ -16,7 +16,7 @@ int main() {
 	int a = 12, b = 25;
 	cout << "a = " << a << endl;
 	cout << "b = " << b << endl;
-	cout << "a | b = " << (a | b) << endl;
+	cout << "a | b = " << (a | b);
 
 	return 0;
 
